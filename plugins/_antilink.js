cb98e7cabdee5f0dc79c98bb0215a81d1626043c
Florcita 🌸 <kaniflorencia@gmail.com>
2024-07-21 17:30:36 -0400

    Actualizar _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -16,8 +16,8 @@ export async function before(m, {conn, isAdmin, isBotAdmin }) {
         }
         await conn.reply(m.chat, `*≡ Enlace Detectado*
             
-No permitimos enlaces de otros grupos 
-lo siento *@${m.sender.split('@')[0]}*  serás expulsado del grupo ${isBotAdmin ? '' : '\n\nNo soy admin así que no te puedo expulsar :"v'}`, null, { mentions: [m.sender] } )
+𝑁𝑜 𝑎𝑐𝑒𝑝𝑡𝑎𝑚𝑜𝑠 𝑒𝑛𝑙𝑎𝑐𝑒𝑠 𝑑𝑒 𝑜𝑡𝑟𝑜𝑠 𝑔𝑟𝑢𝑝𝑜𝑠 
+𝐿𝑜 𝑠𝑖𝑒𝑛𝑡𝑜 *@${m.sender.split('@')[0]}*  𝑠𝑒𝑟𝑎́𝑠 𝑒𝑥𝑝𝑢𝑙𝑠𝑎𝑑𝑜 𝑑𝑒𝑙 𝑔𝑟𝑢𝑝𝑜${isBotAdmin ? '' : '\n\n𝑁𝑜 𝑠𝑜𝑦 𝑎𝑑𝑚𝑖𝑛 𝑎𝑠𝑖́ 𝑞𝑢𝑒 𝑛𝑜 𝑡𝑒 𝑝𝑢𝑒𝑑𝑜 𝑒𝑥𝑝𝑢𝑙𝑠𝑎𝑟 :"v'}`, null, { mentions: [m.sender] } )
         if (isBotAdmin && chat.antiLink) {
         	await conn.sendMessage(m.chat, { delete: m.key })
             await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
