65bc39dcacc52495fe0fa4372889909159ccc590
Florcita 🌸 <kaniflorencia@gmail.com>
2024-07-21 17:42:02 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url' 
 
 global.owner = [
-  ['5493536568522', 'CREADOR 👹', true],
-  ['59168683798', 'COLAB 👹', true],
+  ['56945843173', '𝗖𝗥𝗘𝗔𝗗𝗢𝗥/𝗔 🦨', true],
+  ['59168683798', '𝗖𝗢𝗟𝗔𝗕 🦨', true],
 ] //Numeros de owner 
 
 global.mods = [''] 
@@ -20,12 +20,12 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = '𝗪𝗶𝗲𝗿𝗯𝗼𝘁 𝙋𝙧𝙤𝙛𝙚𝙨𝙨𝙞𝙤𝙣𝙖𝙡' 
-global.author = '@max_xitado.pe' 
+global.packname = '𝗚𝗮𝗮𝗿𝗮𝗕𝗼𝘁' 
+global.author = '@xs.akiraa' 
 
 //--info FG
 global.botName = 'Genesis'
-global.fgig = 'https://instagram.com/max_xitado.pe' 
+global.fgig = 'https://instagram.com/xs.akiraa' 
 global.fgsc = 'https://github.com/karim-off/GenesisBot-Pro' 
 global.fgyt = ''
 global.fgpyp = ''
@@ -38,9 +38,9 @@ global.bgp = 'https://chat.whatsapp.com/LcFTUnvu0Tw1tCnA2ybdR6'
 global.bgp2 = 'https://chat.whatsapp.com/J4GaLNqbzAHEkzNQy9N62O'
 global.bgp3 = 'https://chat.whatsapp.com/J4GaLNqbzAHEkzNQy9N62O' //--GP NSFW
 
-global.wait = '👹 𝘾𝙖𝙧𝙜𝙖𝙣𝙙𝙤....'
+global.wait = '🍁 𝗖𝗮𝗿𝗴𝗮𝗻𝗱𝗼....'
 global.rwait = '⌛'
-global.dmoji = '🤭'
+global.dmoji = '🌚'
 global.done = '✅'
 global.error = '❌' 
 global.xmoji = '🔥' 
